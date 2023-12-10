f43a7552e064e9bc4aa1e2360d912bdac4b400ce
Lucas Cardoso <lucasegbl@gmail.com>
2023-12-09 23:29:04 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone Nokia = new Nokia(numero: "86052080", modelo: "Modelo 1", imei: "15213511", memoria: 128);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Tinder");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "8184621", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Baidu");
